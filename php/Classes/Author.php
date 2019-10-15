e9a7d890cb0722291d3e349c68d83ffcd9376568
Kyla Bendt <kylabendt@gmail.com>
2019-10-15 17:04:50 -0600

    Add variables for id, activationToken and avatarUrl

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -15,7 +15,39 @@ use Ramsey\Uuid\Uuid;
 
 class Author  {
 
+//TODO: Write and document all state variables in the class
 
+/*authorId binary(16) not null,
+authorActivationToken char(32),
+authorAvatarUrl varchar(255),
+authorEmail varchar(128) not null,
+authorHash char(97) not null,
+authorUsername varchar(32) not null,
+unique(authorEmail),
+unique(authorUsername),
+primary key(authorId)*/
 
+/**
+ * id for this author, primary key
+ * @var Uuid $authorId
+ */
+private $authorId;
+/**
+ * activation token for this author, char(32)
+ * @var string
+ */
+private $authorActivationToken;
+/**
+ * avatar url for this author
+ * @var string
+ */
+private $authorAvatarUrl;
+
+//TODO: Write and document an accessor/getter method for each state variable
+
+//TODO: Write and document a mutator/setter method for each state variable
+
+//TODO: Write and document constructor method
 
 }
+
