d042a527878d0760a8a4574e808c06b2e675fdde
Kyla Bendt <kylabendt@gmail.com>
2019-10-15 16:33:54 -0600

    add header info for Author class

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -1,1 +1,21 @@
 <?php
+namespace KylaBendt\ObjectOrientedProgramming;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__) ."/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ * This class represents an author (who is also a user)
+ *
+ * @author Kyla Bendt
+ * @version 0.0.1
+ */
+
+class Author  {
+
+
+
+
+}
